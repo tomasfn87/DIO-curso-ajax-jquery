7ece5a173e1e5df853e8c7d9565d7e95c37d6450
tomasfn87 <tomas.nalle@alumni.usp.br>
2021-10-11 11:10:52 -0300

    Funcionando: 1): "<input id="cep">" e 2): "<button>": 1): recebe um CEP a ser buscado em "https://viacep.com.br/ws/${cep}/json/"; 2): executa a ação, e o resultado é retornado no corpo do documento.

--- js/main.js
+++ js/main.js
@@ -1,1 +1,21 @@
 // alert("Funcionou!")
+function consultaCep() {
+    let cep = document.getElementById("cep").value
+    let url = `https://viacep.com.br/ws/${cep}/json/`
+    $.ajax({
+        url: url,
+        type: "GET",
+        success: function(response) {
+            console.log(response)
+            $("#logradouro").html(`${response.logradouro}`)
+            $("#bairro").html(`${response.bairro}`)
+            $("#localidade").html(`${response.localidade}/`)
+            $("#uf").html(`${response.uf}`)
+            /* document.getElementById("logradouro").innerHTML = response.logradouro + ","
+            document.getElementById("bairro").innerHTML = response.bairro + ","
+            document.getElementById("localidade").innerHTML = response.localidade + "/"
+            document.getElementById("uf").innerHTML = response.uf */ 
+            // mantendo comentários acima para fins de aprendizado
+        }
+    })
+} 
